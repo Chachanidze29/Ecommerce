f4f7e8537fefe765f14b23b7ad539ac0357167b9
Chachanidze29 <chachanidze29m@gmail.com>
2024-09-17 12:29:05 +0400

    remove obsolete field

--- resources/js/types/models.ts
+++ resources/js/types/models.ts
@@ -2,7 +2,6 @@ export type Category = {
     id: number;
     name: string;
     description: string;
-    parent_id: number;
     products: Product[];
     sub_categories: Category[];
     parent_category: Category;
